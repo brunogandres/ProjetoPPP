8e51047604fc695d6c6db92da54e83ea7dcac738
brunogandres <49528108+brunogandres@users.noreply.github.com>
2019-05-07 19:26:33 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
 
 typedef struct PdI *LPdI;
 typedef struct PdI{
@@ -30,13 +29,11 @@ typedef struct User{
 }UserData;
 
 
-
-
 /*cabeçalhos*/
-void menu();
-void registo();
+void menu(LUser lista);
+void registo(LUser lista);
 void alterardados();
-void ordemalfabetica(Lcidade lista);
+void carrega_listaCidades_ficheiro(Lcidade lista);
 
 Lcidade cria_lista_CIDADES(void);
 int lista_vazia_CIDADES(Lcidade lista);
@@ -51,24 +48,18 @@ void procura_lista_PDIS (LPdI lista, char *chave, LPdI *ant, LPdI *actual);
 void imprime_lista_PDIS (LPdI lista);
 void insere_lista_PDIS (LPdI lista, char *it);
 
+//LUser cria_lista_USERS (UserData user);
 LUser cria_lista_USERS (void);
 LUser destroi_lista_USERS (LUser lista);
 int lista_vazia_USERS(LUser lista);
 void procura_lista_USERS (LUser lista, char chave[], LUser *ant, LUser *actual);
-LUser insere_lista_USERS(LUser lista, char it[]);
+void insere_lista_USERS (LUser lista, UserData  user);
 void imprime_lista_USERS (LUser lista);
 
-
-LUser2 cria_lista_USERS2(void);
-LUser2 destroi_lista_USERS2 (LUser2 lista);
-int lista_vazia_USERS2(LUser2 lista);
-void procura_lista_USERS2 (LUser2 lista, char chave[], LUser2 *ant, LUser2 *actual);
-void insere_lista_USERS2 (LUser2 lista, char it[]);
-void imprime_lista_USERS2 (LUser2 lista);
-
-
-
-
+void carrega_listaUsers_ficheiro(LUser lista, char *nomeFicheiroUsers, Lcidade listaC);
+void grava_listaUsers_ficheiro(LUser lista, char *nomeFicheiroUsers);
+void getUserPrefCidades(char *linha, char *pref1, char *pref2, char *pref3);
+Lcidade getNoCidade(Lcidade listaC, char *pref);
 
 
 
@@ -76,128 +67,189 @@ void imprime_lista_USERS2 (LUser2 lista);
 
 int main() {
     Lcidade lista_cidades;
+    LUser lista_users;
+    char ficheiroUsers[] =  "C:\\Users\\Utilizador\\Desktop\\Projeto\\Dados.txt";
     lista_cidades = cria_lista_CIDADES();//lista criada aqui caso seja precisa pra outras coisas
-    LUser  lista_users = cria_lista_USERS();
-    LUser2 lista_users_detalhes = cria_lista_USERS2();
-    //menu();
-    alterardados();
-    //ordemalfabetica(lista_cidades);
+    lista_users = cria_lista_USERS();
+    carrega_listaCidades_ficheiro(lista_cidades);
+
+    carrega_listaUsers_ficheiro(lista_users, ficheiroUsers, lista_cidades);
+
+    //menu(lista_users);
+    //gravar para ficheiros a lista utilizadores
+
+    //registo(lista_users);
+
+    //grava_listaUsers_ficheiro(lista_users, ficheiroUsers);
+    //DEPOIS DE GRAVAR É PRECISO FAZER O FREE DAS LISTAS
+    //alterardados();
+    //
+
 
 }
 
 
-void menu(){
+void menu(LUser lista){
 
-        int n;
-        printf("\n**********************************************");
-        printf("\n\t\t\tBEM VINDO");
-        printf("\n**********************************************");
-        printf("\n1. Novo Registo");
-        printf("\n2. Alterar Dados");
-        printf("\n3. Sair");
-        printf("\n**********************************************");
-        printf("\nPlease enter your option< 1 / 2 / 3 >: ");
-        scanf("%d", &n);
+    int n;
+    printf("\n**********************************************");
+    printf("\n\t\t\tBEM VINDO");
+    printf("\n**********************************************");
+    printf("\n1. Novo Registo");
+    printf("\n2. Alterar Dados");
+    printf("\n3. Sair");
+    printf("\n**********************************************\n");
+    printf("Escolha uma opcao: < 1 / 2 / 3 >: ");
+    fflush(stdin);
+    scanf("%d", &n);
 
-        switch(n){
-            case 1:
-                registo();
+    switch(n){
+        case 1:
+            registo(lista);
+            break;
+        case 2:
+            alterardados();
+            break;
+        case 3:
+            printf("\n*** Obrigado por usar o programa. ***");
 
-            case 2:
-                alterardados();
-                break;
-            case 3:
-                printf("\n*** Obrigado por usar o programa. ***");
-                exit(0);
-                break;
-            default:
-                printf("\nErro! Numero Errado\nTente outra vez");
-                menu();
+            break;
+        default:
+            printf("\nErro! Numero Errado\nTente outra vez");
 
-                break;
-        }
+            break;
+    }
 }
 
-/* REGISTO E GUARDA NUM FICHEIRO*/
-void registo(){
+
+void carrega_listaUsers_ficheiro(LUser lista, char *nomeFicheiroUsers, Lcidade listaC) {
+    char linha[256];
+    char pref1[100], pref2[100], pref3[100];
+    int i, contador = 0, len_frase;
+    LUser noUser = NULL;
+
+    UserData tempUser; //estrutura temporaria para guardar as informacoes
+    FILE *ficheiroDados = fopen(nomeFicheiroUsers, "r");
 
 
-    UserData user1;
-    printf("\n\t ####REGISTO####\n");
-    printf("Nome: ");
-    gets(user1.nome);
-    printf("\nMorada: ");
-    gets(user1.morada);
-    printf("\nData de Nascimento: ");
-    gets(user1.data_nascimento);
-    printf("Telemovel: ");
-    gets(user1.telemovel);
+    if(ficheiroDados != NULL) {
+        for (i = 0; fgets(linha, 256, ficheiroDados); i++) {
+            len_frase = strlen(linha);
+            if (linha[len_frase - 1] == '\n') {
+                linha[len_frase - 1] = '\0';
+            }
 
-
-
-    FILE *userdados = fopen("C:\\Users\\Utilizador\\Desktop\\Projeto\\Dados.txt", "a");
-
-    fprintf(userdados, "*%s\n", user1.nome);
-    fprintf(userdados, "%s\n", user1.morada);
-    fprintf(userdados, "%s\n", user1.data_nascimento);
-    fprintf(userdados, "%s\n", user1.telemovel);
-    printf("\nSYSTEM: Registado com Sucesso.");
-
-    fclose(userdados);
-
-}
-void alterardados() {
-    LUser user1;
-    LUser utilizador;
-    char linha[256], *nome;
-    int i, contador = 0;
-    int len_frase;
-    int len;
-    FILE *userdados = fopen("C:\\Users\\Utilizador\\Desktop\\Projeto\\Dados.txt", "r");
-    FILE *userdados2 = fopen("C:\\Users\\Utilizador\\Desktop\\Projeto\\DadosCopia.txt", "a");
-    char aux[len];
-    printf("Nome: ");
-    gets(user1->nome);
-
-    for (i = 1; fgets(linha, 256, userdados) != NULL; i++) {
-        len_frase = strlen(linha);
-        if (linha[len_frase - 1] == '\n') {
-            linha[len_frase - 1] = '\0';
-        }
-        if(contador == 3) {
-            contador = 0;
-        }
-        else if(contador == 0)
-        {
-            fprintf(userdados2, "%s\n", linha);
-            if(strcmp(linha+1,user1->nome) == 0)
+            if (linha[0] == '*')
             {
-                contador++;
-                printf("Morada: ");
-                gets(user1->morada);
-                fprintf(userdados2, "%s\n", user1 -> morada);
-                printf("Data de Nascimento: ");
-                gets(user1 ->data_nascimento);
-                fprintf(userdados2, "%s\n", user1 -> data_nascimento);
-                printf("Telemovel: ");
-                gets(user1 ->telemovel);
-                fprintf(userdados2, "%s\n", user1 -> telemovel);
+                if (contador != 0)
+                {
+                    insere_lista_USERS(lista, tempUser);
+                }
+                strcpy(tempUser.nome, linha + 1);
+                contador = 1;
+
+            } else{
+                switch (contador)
+                {
+                    case 1:
+                        strcpy(tempUser.morada, linha);
+                        contador++;
+                        break;
+                    case 2:
+                        strcpy(tempUser.data_nascimento, linha);
+                        contador++;
+                        break;
+                    case 3:
+                        strcpy(tempUser.telemovel, linha);
+                        contador++;
+
+                        break;
+                    case 4:
+                        //Obter as preferencias
+                        getUserPrefCidades(linha, pref1, pref2, pref3);
+                        tempUser.prefLocal1 = getNoCidade(listaC, pref1);
+                        tempUser.prefLocal2 = getNoCidade(listaC, pref2);
+                        tempUser.prefLocal3 = getNoCidade(listaC, pref3);
+                        printf("\nPreferencia : %s- %s-%s-%s", tempUser.prefLocal1->nome_cidade,tempUser.prefLocal2->nome_cidade, tempUser.prefLocal3->nome_cidade);
+                        contador++;
+
+                }
+
             }
         }
-
-        else
-            contador+=1;
+        insere_lista_USERS(lista, tempUser);
     }
-    fclose(userdados);
-    fclose(userdados2);
-    unlink("C:\\Users\\Utilizador\\Desktop\\Projeto\\Dados.txt");
-    rename("C:\\Users\\Utilizador\\Desktop\\Projeto\\DadosCopia.txt", "C:\\Users\\Utilizador\\Desktop\\Projeto\\Dados.txt");
-    printf("\nSYSTEM: Utilizador alterado com Sucesso.");
+}
+void getUserPrefCidades(char *linha, char *pref1, char *pref2, char *pref3)
+{
+    char barra[] = "|";
+    strcpy(pref1,strtok(linha, barra));
+    strcpy(pref2,strtok(NULL, barra));
+    strcpy(pref3,strtok(NULL, barra));
+}
+
+Lcidade getNoCidade(Lcidade listaC, char *pref){
+    Lcidade l = listaC->next;
+    int i, len_frase = strlen(pref), len_frase1;
+    if(strcmp(pref, " ") == 0){
+        return NULL;
+    }
+    while (l)
+    {
+        if(strcmp(l->nome_cidade, pref) == 0){
+            return l;
+        }
+        else
+            l = l->next;
+    }
+}
+void grava_listaUsers_ficheiro(LUser lista, char *nomeFicheiroUsers)
+{
+    FILE *dados = fopen(nomeFicheiroUsers, "w");
+    LUser l = lista->next;
+    while (l != NULL)
+    {
+        fprintf(dados, "*%s\n", l->nome);
+        fprintf(dados, "%s\n", l->morada);
+        fprintf(dados, "%s\n", l->data_nascimento);
+        fprintf(dados, "%s\n", l->telemovel);
+        l = l->next;
+    }
+}
+
+
+/* REGISTO E GUARDA NUM FICHEIRO*/
+void registo(LUser lista){
+
+    UserData user;
+
+    printf("\n\t ####REGISTO####");
+    printf("\n");
+    printf("Nome:");
+    gets(user.nome);
+    printf("\nMorada: ");
+    gets(user.morada);
+    printf("\nData de Nascimento: ");
+    gets(user.data_nascimento);
+    printf("Telemovel: ");
+    gets(user.telemovel);
+
+    insere_lista_USERS(lista, user);
+}
+
+
+
+
+
+void alterardados() {
 
 }
 
-void ordemalfabetica(Lcidade lista) {
-    int i;
+
+
+
+void carrega_listaCidades_ficheiro(Lcidade lista) {
+    int i, len;
     char leitura[256];
     FILE *cidades = fopen("C:\\Users\\Utilizador\\Desktop\\Projeto\\Cidades.txt", "r");
     Lcidade noLocal = NULL;//ponteiro para correr cada nó da lista de cidades
@@ -205,14 +257,17 @@ void ordemalfabetica(Lcidade lista) {
 
     for (i = 0; fgets(leitura, 256, cidades)!=NULL; i++){
 
-        if (leitura[0] == '*') {
+        if (leitura[0] == '*' ) {
+            len = strlen(leitura);
+            if(leitura[len-1] == '\n')
+                leitura[len - 1] = '\0';
             noLocal = insere_lista_CIDADES(lista, leitura + 1);
         }
         else {
             insere_lista_PDIS(noLocal->pontos, leitura+1);
         }
     }
-    imprime_lista_CIDADES(lista);
+    //imprime_lista_CIDADES(lista);
     fclose(cidades);
 }
 
@@ -274,7 +329,7 @@ Lcidade insere_lista_CIDADES(Lcidade lista, char it[]) {
 void imprime_lista_CIDADES (Lcidade lista) {
     Lcidade l = lista->next; /* Salta o header */
     while (l) {
-        printf("%s ", l->nome_cidade);
+        printf("%s", l->nome_cidade);
         imprime_lista_PDIS(l->pontos);
         l=l->next;
     }
@@ -332,7 +387,7 @@ void imprime_lista_PDIS (LPdI lista) {
     LPdI l = lista->next; /* Salta o header */
     while (l) {
         printf("\t• %s", l->nome);
-        l=l->next;
+        l = l->next;
     }
 }
 
@@ -340,14 +395,19 @@ void imprime_lista_PDIS (LPdI lista) {
 
 //LISTA USERS
 
-
-
 LUser cria_lista_USERS (void) {
     LUser aux;
     aux = (LUser) malloc (sizeof (UserData));
     if (aux != NULL) {
         strcpy(aux->nome, "");
-        aux->detalhes = cria_lista_USERS2();
+        strcpy(aux->morada, "");
+        strcpy(aux->data_nascimento, "");
+        strcpy(aux->telemovel, "");
+        aux->prefLocal1 = NULL;
+        aux->prefLocal2 = NULL;
+        aux->prefLocal3 = NULL;
+        aux->prefPontos = NULL;
+
         aux->next = NULL;
     }
     return aux;
@@ -360,101 +420,62 @@ LUser destroi_lista_USERS (LUser lista) {
     }
     free(lista);
     return NULL;
+
 }
 int lista_vazia_USERS(LUser lista) {
     return (lista->next == NULL ? 1 : 0);
 }
-void procura_lista_USERS (LUser lista, char chave[], LUser *ant, LUser *actual) {
+
+void procura_lista_USERS (LUser lista, char chave[], LUser *ant, LUser *actual)
+{
     *ant = lista;
     *actual = lista->next;
-    while ((*actual) != NULL && strcmp((*actual)->nome, chave) < 0) {
+    while ((*actual) != NULL && (*actual)->nome < chave)
+    {
         *ant = *actual;
         *actual = (*actual)->next;
     }
-    if ((*actual) != NULL && strcmp((*actual)->nome, chave))
-        *actual = NULL;
+    if ((*actual) != NULL && (*actual)->nome != chave)
+        *actual = NULL;   /* Se elemento não encontrado*/
 }
+void insere_lista_USERS (LUser lista, UserData  user) {
+    LUser no ;
+    LUser ultimo, anterior;
 
-LUser insere_lista_USERS(LUser lista, char it[]) {
-    LUser no;
-    LUser ant, inutil;
-    no = (LUser) malloc (sizeof (UserData));//reserva espaço para o nó
+
+    no = (LUser) malloc (sizeof (UserData));
     if (no != NULL) {
-        strcpy(no->nome, it);
-        procura_lista_USERS (lista, it, &ant, &inutil);
-        no->detalhes = cria_lista_USERS2();
-        no->next = ant->next;
-        ant->next = no;
+        strcpy(no->nome, user.nome);
+        strcpy(no->morada, user.morada);
+        strcpy(no->data_nascimento, user.data_nascimento);
+        strcpy(no->telemovel, user.telemovel);
+        no->next = NULL;
     }
-    return no;
+
+    // introduzir na lista
+    if(lista->next == NULL)
+    {
+        lista->next = no;
+    }
+    else  //encontrar o ultimo da lista e inserir o no no fim
+    {
+        anterior = lista;
+        ultimo = lista->next; /* Salta o header */
+        while (ultimo)
+        {
+            anterior = ultimo;
+            ultimo = ultimo->next;
+        }
+        anterior->next = no;
+    }
 }
 void imprime_lista_USERS (LUser lista) {
     LUser l = lista->next; /* Salta o header */
     while (l) {
         printf("%s\n", l->nome);
-        imprime_lista_USERS2(l->detalhes);
-        l=l->next;
-    }
-}
-
-
-
-//lista para os detalhes LUSERS2
-
-
-
-LUser2 cria_lista_USERS2 (void) {
-    LUser2 aux;
-    aux = (LUser2) malloc (sizeof (UserData2));
-    if (aux != NULL) {
-        strcpy(aux->morada, "");
-        strcpy(aux->data_nascimento, "");
-        strcpy(aux->telemovel, "");
-        aux->next = NULL;
-    }
-    return aux;
-}
-
-LUser2 destroi_lista_USERS2 (LUser2 lista) {
-    LUser2 temp_ptr;
-    while (lista_vazia_USERS2(lista) == 0) {
-        temp_ptr = lista;
-        lista= lista->next; free (temp_ptr);
-    }
-    free(lista);
-    return NULL;
-
-}
-int lista_vazia_USERS2(LUser2 lista) {
-    return (lista->next == NULL ? 1 : 0);
-}
-void procura_lista_USERS2 (LUser2 lista, char chave[], LUser2 *ant, LUser2 *actual) {
-    *ant = lista;
-    *actual = lista->next;
-    while ((*actual) != NULL && strcmp((*actual)->morada, chave) < 0) {
-        *ant = *actual;
-        *actual = (*actual)->next;
-    }
-    if ((*actual) != NULL && strcmp((*actual)->morada, chave))
-        *actual = NULL;
-}
-void insere_lista_USERS2 (LUser2 lista, char it[]) {
-    LUser2 no ;
-    LUser2 ant, inutil;
-    no = (LUser2) malloc (sizeof (UserData2));
-    if (no != NULL) {
-        strcpy(no->morada, it);
-        procura_lista_USERS2(lista, it, &ant, &inutil);
-        no->next = ant->next;
-        ant->next = no;
-    }
-}
-void imprime_lista_USERS2 (LUser2 lista) {
-    LUser2 l = lista->next; /* Salta o header */
-    while (l) {
-        printf("%s", l->morada);
-        printf("%s", l->data_nascimento);
-        printf("%s", l->telemovel);
-        l=l->next;
+        printf("%s\n", l->morada);
+        printf("%s\n", l->data_nascimento);
+        printf("%s\n", l->telemovel);
+        l = l->next;
     }
 }
